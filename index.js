a75b2e106f3c4e5f1d2a934a962662e5f66a2ba1
Satyam gupta <stymngrya@gmail.com>
2022-11-04 11:52:20 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+require("dotenv").config()
+app.use(express.json());
+
+const PORT = process.dotenv.PORT || 8500
+app.get("/",(req,res)=>{
+
+    res.send("home page")
+})
+
+
+app.listen(PORT,()=>{
+    console.log("listening on : "+PORT)
+})
